07abe14f068c47f5ac50f42d5e214b33b4ce4369
qingyu1229 <705203544@qq.com>
2015-03-04 17:58:22 +0800

    3-4

---
+++ hhxxttxs/src/main/java/com/httpclient/CheckUrl.java
@@ -0,0 +1,68 @@
+package com.httpclient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+public class CheckUrl {
+
+	public static void main(String[] args) throws IOException {
+		FileOutputStream fos=new FileOutputStream(	new File("D:\\test3.xlsx"));
+		XSSFWorkbook wfb = new XSSFWorkbook();
+		XSSFSheet sheet1= wfb.createSheet("s1");
+		int count=0;
+		FileInputStream in = new FileInputStream("D:\\test.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(in);
+		// XSSFSheet sheet = wb.getSheetAt(0);
+		
+		XSSFSheet sheet = wb.getSheetAt(0);
+		int totalRows = sheet.getLastRowNum(); 
+		for(int i=1;i<totalRows;i++){
+			String str= sheet.getRow(i).getCell(2).getStringCellValue();
+			System.out.println("访问URL:"+str);
+			boolean bl= checkUrl(str);
+			if(!bl){
+				System.out.println("访问出错："+str);
+				sheet1.createRow(count++).createCell(0).setCellValue(str);
+			}
+		}
+		wfb.write(fos);
+		fos.flush();
+		fos.close();
+		wfb.close();
+	}
+	
+	public static boolean checkUrl(String url){
+		try {
+			Document doc= Jsoup.connect(url).timeout(5000).get();
+			if(doc!=null){
+				return true;
+			}
+		} catch (IOException e) {
+			return false;
+			//e.printStackTrace();
+		}
+		return false;
+	}
+	
+/*	
+	public static void main(String[] args) throws IOException{
+		FileOutputStream fos=new FileOutputStream(	new File("D:\\test2.xlsx"));
+		XSSFWorkbook wb = new XSSFWorkbook();
+		XSSFSheet sheet= wb.createSheet("s1");
+		sheet.createRow(0).createCell(0).setCellValue("ddddddddddddddddddddddd");
+		wb.write(fos);
+		fos.flush();
+		fos.close();
+		wb.close();
+	}*/
+	
+
+}
--- hhxxttxs/src/main/java/com/ipangu/Test.java
+++ hhxxttxs/src/main/java/com/ipangu/Test.java
@@ -1,4 +1,4 @@
-package com.httpclient;
+package com.ipangu;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -8,24 +8,23 @@ import java.util.List;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 
 public class Test {
 	
+	HttpClient httpClient;
 	public static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.151 Safari/535.19";  
-	public static void main(String[] args) throws ClientProtocolException, IOException {
-		
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost httpost = new HttpPost("http://my.oschina.net/u/1032350/fellow");
+	
+	public String fetch() throws ParseException, IOException{
+		HttpPost httpost = new HttpPost("http://m.ctrip.com/restapi/soa2/10324/hotel/product/roomget");
 		List<NameValuePair> nvps = new ArrayList <NameValuePair>();
-		/*nvps.add(new BasicNameValuePair("biz", "1"));
+		nvps.add(new BasicNameValuePair("biz", "1"));
 		nvps.add(new BasicNameValuePair("contentType", "json"));
 		nvps.add(new BasicNameValuePair("contrl", "2"));
 		nvps.add(new BasicNameValuePair("icldrid", "0"));
@@ -37,19 +36,16 @@ public class Test {
 		nvps.add(new BasicNameValuePair("cityId", "0"));
 		nvps.add(new BasicNameValuePair("dstId", "0"));
 		nvps.add(new BasicNameValuePair("inDay", "2015-02-02"));
-		nvps.add(new BasicNameValuePair("outDay", "2015-02-03"));*/
-		
-		//nvps.add(new BasicNameValuePair("Cookie", "oscid=WEn2ruDRkA5zbxnHQ2DapdSxQXpnogPfPDSuDwCfdaW%2F8jJlXoSyupJfptQ5iaclJuN9iX9FbAjjstJ8WXfDID4xKE4nmcY97An2TmYXIdeR5jJEXDMwADTEXOJ70Zi8"));
+		nvps.add(new BasicNameValuePair("outDay", "2015-02-03"));
 		
 		httpost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
-		httpost.setHeader("Cookie", "oscid=WEn2ruDRkA5zbxnHQ2DapdSxQXpnogPfPDSuDwCfdaW%2F8jJlXoSyupJfptQ5iaclJuN9iX9FbAjjstJ8WXfDID4xKE4nmcY97An2TmYXIdeR5jJEXDMwADTEXOJ70Zi8");
-		
-		HttpResponse response = httpclient.execute(httpost);
+		HttpResponse response = httpClient.execute(httpost);
 		HttpEntity entity = response.getEntity();
 		int statusCode= response.getStatusLine().getStatusCode();
 		System.out.println(statusCode);
 		String s= EntityUtils.toString(entity);
 		System.out.println(s);
 		
+		return "";
 	}
 }
